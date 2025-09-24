5a9f833083890006918f496b37c46d0379337dea
Yuri <ygonzoo@gmail.com>
2025-09-25 01:23:25 +0200

    fix helloworld

--- sketches/helloworld/sketch.js
+++ sketches/helloworld/sketch.js
@@ -17,45 +17,29 @@ function draw() {
   // strokeWeight(5)
   // triangle(50,30,100,200,400,400);
 
-  for (let i = 0; i < 400; i = i + 20) {
-    for (let j = 0; j < 400; j = j + 20) {
+  for (let i = -10; i <= width + 10; i = i + 20) {
+    for (let j = -10; j <= height + 10; j = j + 20) {
       textSize(20);
-      text("🦄", i, j);
+      text("⚫", i, j);
     }
   }
-
-  // clip(mask1);
-
+  clip(mask1);
   noStroke();
   rect(100, 50, 200, 200);
   ellipse(200, 250, 200, 200);
-
   fill("black");
   rect(200 - 5, 50, 10, 300);
-
   rect(100, 175, 200, 10);
-
   textSize(75);
   text("🦄", 100, 150);
-
   rotatedText();
-
   noFill();
-
   stroke(24, 33, 150);
-
-  strokeWeight(5);
-  circle(200, 200, 400);
-
-  fill("red");
-  // rect(0,0,width,height)
 }
 
 function mask1() {
-  rect(250, 0, 50, 400);
-  circle(200, 150, 100);
-  rect(100, 0, 50, 400);
-  circle(200, 300, 200);
+  strokeWeight(4);
+  circle(200, 180, width / 1.3);
 }
 
 function rotatedText() {
@@ -63,7 +47,6 @@ function rotatedText() {
   textSize(75);
   translate(300, 100);
   rotate(PI);
-  square(0, 0, 75);
   text("🦄", 0, 0);
   pop();
 }
