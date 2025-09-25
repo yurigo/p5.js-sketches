cfcb0e2b280bf60ff31056d74320187ed4447bfc
Yuri <ygonzoo@gmail.com>
2025-09-25 17:42:48 +0200

    fix: draw function missing in random-chaos

--- sketches/random-chaos/sketch.js
+++ sketches/random-chaos/sketch.js
@@ -3,14 +3,16 @@ function setup() {
   createCanvas(s, s);
   noStroke();
   fill(200, 100, 255, 150);
-
-  for (let i = 0; i < width; i += 20) {
-    ellipse(i, random(height), 15);
-  }
 }
 
 function windowResized() {
   const s = min(windowWidth, windowHeight) * 0.9;
   resizeCanvas(s, s);
   redraw();
+}
+
+function draw(){
+    for (let i = 0; i < width; i += 20) {
+      ellipse(i, random(height), 15);
+    }
 }
