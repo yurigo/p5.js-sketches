646ea8680ce77b7b52b6ffff2db40bb8705228ba
Yuri <ygonzoo@gmail.com>
2025-10-23 17:55:11 +0200

    fix: handleMottion

--- sketches/waaac-woooc/sketch.js
+++ sketches/waaac-woooc/sketch.js
@@ -4,23 +4,28 @@
 
 var x, y, z;
 
-function setup(){
+function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-	x = 0;
+  x = 0;
   y = 0;
   z = 0;
 
-
   // Request permission for iOS 13+
-  if (typeof DeviceOrientationEvent !== "undefined" && typeof DeviceOrientationEvent.requestPermission === "function") {
+  if (
+    typeof DeviceOrientationEvent !== "undefined" &&
+    typeof DeviceOrientationEvent.requestPermission === "function"
+  ) {
     DeviceOrientationEvent.requestPermission()
-      .then(permissionState => {
+      .then((permissionState) => {
         if (permissionState === "granted") {
           window.addEventListener("devicemotion", handleMotion);
         } else {
           // Display a message asking the user to grant permission in settings
           createButton("Allow access to sensors").mousePressed(() => {
-            window.open("https://support.apple.com/guide/iphone/settings-34a528f7a3f2/ios", "_blank");
+            window.open(
+              "https://support.apple.com/guide/iphone/settings-34a528f7a3f2/ios",
+              "_blank"
+            );
           });
         }
       })
@@ -29,24 +34,28 @@ function setup(){
     // Handle cases for devices older than iOS 13 or browsers that don't support the API
     window.addEventListener("devicemotion", handleMotion);
   }
-
-
 }
 
-function draw(){
+function draw() {
   background(255, 255, 255, 255);
-  translate(-width/2, 0, -600);
-  
+  translate(-width / 2, 0, -600);
+
   // rotate the box based on accelerometer data
   // we could use rotationX,Y here but demonstrating
   // acceleration
-  x+=accelerationX*0.05;
-  y+=accelerationY*0.05;
-  z+=accelerationZ*0.05;
+  x += accelerationX * 0.05;
+  y += accelerationY * 0.05;
+  z += accelerationZ * 0.05;
   normalMaterial();
   rotateX(x);
   rotateY(y);
   rotateZ(z);
   box(200, 200, 200);
+}
 
+function handleMotion(e) {
+  // Use accelerationIncludingGravity for more consistent results on iOS
+  x = parseInt(e.accelerationIncludingGravity.x);
+  y = parseInt(e.accelerationIncludingGravity.y);
+  z = parseInt(e.accelerationIncludingGravity.z);
 }
