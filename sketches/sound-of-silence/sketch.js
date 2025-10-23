8d848e4feb793a0a01811bb70474424f128c555c
Yuri <ygonzoo@gmail.com>
2025-10-23 20:29:48 +0200

    chore: cannon in D

--- sketches/sound-of-silence/sketch.js
+++ sketches/sound-of-silence/sketch.js
@@ -16,24 +16,135 @@ function draw() {
   background(45, 52, 54);
 }
 
-function mousePressed(){
-  playSynth()
+function mousePressed() {
+  playSynth();
 }
 
 function playSynth() {
   userStartAudio();
 
   // note duration (in seconds)
-  let dur = 1.5;
+  let dur = 0.1;
 
   // time from now (in seconds)
   let time = 0;
 
   // velocity (volume, from 0 to 1)
-  let vel = 0.1;
+  let vel = 0.4;
 
   // notes can overlap with each other
-  polySynth.play('G2', vel, 0, dur);
-  polySynth.play('C3', vel, time += 1/3, dur);
-  polySynth.play('G3', vel, time += 1/3, dur);
+  // G chord
+
+  while (time < 10) {
+    // polySynth.play("G3", vel, time, dur);
+    // time = time + dur + 0.1; // add a small gap between chords
+    // polySynth.play("B4", vel, time, dur);
+    // time = time + dur + 0.1; // add a small gap between chords
+    // polySynth.play("D4", vel, time, dur);
+    // // B chord
+    // time = time + dur + 0.1; // add a small gap between chords
+    // polySynth.play("B4", vel, time, dur);
+    // time = time + dur + 0.1; // add a small gap between chords
+    // polySynth.play("D4", vel, time, dur);
+    // time = time + dur + 0.1; // add a small gap between chords
+    // polySynth.play("F4", vel, time, dur);
+    // // C chord
+    // time = time + dur + 0.1;
+    // polySynth.play("C4", vel, time, dur);
+    // time = time + dur + 0.1; // add a small gap between chords
+    // polySynth.play("E4", vel, time, dur);
+    // time = time + dur + 0.1; // add a small gap between chords
+    // polySynth.play("G4", vel, time, dur);
+    // // Cm chord
+    // time = time + dur + 0.1;
+    // polySynth.play("C4", vel, time, dur);
+    // time = time + dur + 0.1;
+    // polySynth.play("D#4", vel, time, dur);
+    // time = time + dur + 0.1;
+    // polySynth.play("G4", vel, time, dur);
+
+    // G
+
+    polySynth.play("C4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("E4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("G4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("C5", vel, time, dur);
+    time = time + dur + 0.1; // add a small gap between chords
+
+    polySynth.play("C4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("E4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("G4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("B5", vel, time, dur);
+    time = time + dur + 0.1; // add a small gap between chords
+    // add a small gap between chords
+
+    polySynth.play("A4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("C4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("E4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("A5", vel, time, dur);
+    time = time + dur + 0.1; // add a small gap between chords
+
+    polySynth.play("A4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("C4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("E4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("G4", vel, time, dur);
+    time = time + dur + 0.1; // add a small gap between chords
+
+    polySynth.play("F3", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("A4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("C4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("F4", vel, time, dur);
+    time = time + dur + 0.1; // add a small gap between chords
+
+    polySynth.play("F3", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("A4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("C4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("E4", vel, time, dur);
+    time = time + dur + 0.1; // add a small gap between chords
+
+    // polySynth.play("A4", vel, time, dur);
+    // time = time + dur + 0.1; // add a small gap between chords
+    // polySynth.play("C4", vel, time, dur);
+    // time = time + dur + 0.1; // add a small gap between chords
+    // polySynth.play("E4", vel, time, dur);
+    // time = time + dur + 0.1; // add a small gap between chords
+    // polySynth.play("G4", vel, time, dur);
+    // time = time + dur + 0.1; // add a small gap between chords
+
+    polySynth.play("F3", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("A4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("C4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("F4", vel, time, dur);
+    time = time + dur + 0.1; // add a small gap between chords
+
+    polySynth.play("A4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("C4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("E4", vel, time, dur);
+    time = time + dur + 0.01; // add a small gap between chords
+    polySynth.play("G4", vel, time, dur);
+    time = time + dur + 0.1; // add a small gap between chords
+  }
 }
