39315892ce1d62b86e3eb373c6de944e0856c31e
Yuri <ygonzoo@gmail.com>
2025-10-30 16:28:05 +0100

    fix: update visuals A4 is A5, etc in oscillator

--- sketches/oscillator-happy-birthday/sketch.js
+++ sketches/oscillator-happy-birthday/sketch.js
@@ -4,16 +4,43 @@ let isPlaying = false;
 // Happy Birthday melody
 // Format: [note, duration in ms]
 const melody = [
-  ['C4', 400], ['C4', 200], ['D4', 600], ['C4', 600], ['F4', 600], ['E4', 1200],
-  ['C4', 400], ['C4', 200], ['D4', 600], ['C4', 600], ['G4', 600], ['F4', 1200],
-  ['C4', 400], ['C4', 200], ['C5', 600], ['A4', 600], ['F4', 600], ['E4', 600], ['D4', 1200],
-  ['Bb4', 400], ['Bb4', 200], ['A4', 600], ['F4', 600], ['G4', 600], ['F4', 1200]
+  ["C4", 400],
+  ["C4", 200],
+  ["D4", 600],
+  ["C4", 600],
+  ["F4", 600],
+  ["E4", 1200],
+  ["C4", 400],
+  ["C4", 200],
+  ["D4", 600],
+  ["C4", 600],
+  ["G4", 600],
+  ["F4", 1200],
+  ["C4", 400],
+  ["C4", 200],
+  ["C5", 600],
+  ["A5", 600],
+  ["F4", 600],
+  ["E4", 600],
+  ["D4", 1200],
+  ["Bb5", 400],
+  ["Bb5", 200],
+  ["A5", 600],
+  ["F4", 600],
+  ["G4", 600],
+  ["F4", 1200],
 ];
 
 // Note frequencies in Hz
 const noteFreqs = {
-  'C4': 261.63, 'D4': 293.66, 'E4': 329.63, 'F4': 349.23,
-  'G4': 392.00, 'A4': 440.00, 'Bb4': 466.16, 'C5': 523.25
+  C4: 261.63,
+  D4: 293.66,
+  E4: 329.63,
+  F4: 349.23,
+  G4: 392.0,
+  A5: 440.0,
+  Bb5: 466.16,
+  C5: 523.25,
 };
 
 let currentNote = 0;
@@ -23,12 +50,12 @@ let particles = [];
 function setup() {
   const s = min(windowWidth, windowHeight) * 0.9;
   createCanvas(s, s);
-  
+
   // Create oscillator
-  osc = new p5.Oscillator('sine');
+  osc = new p5.Oscillator("sine");
   osc.amp(0);
   osc.start();
-  
+
   textAlign(CENTER, CENTER);
   textSize(24);
 }
@@ -40,7 +67,7 @@ function windowResized() {
 
 function draw() {
   background(15, 15, 35);
-  
+
   // Draw particles
   for (let i = particles.length - 1; i >= 0; i--) {
     particles[i].update();
@@ -49,21 +76,21 @@ function draw() {
       particles.splice(i, 1);
     }
   }
-  
+
   // Draw title
   fill(255, 215, 0);
   noStroke();
-  text('🎵 Happy Birthday Song 🎵', width / 2, 60);
-  
+  text("🎵 Happy Birthday Song 🎵", width / 2, 60);
+
   fill(255, 165, 0);
   textSize(16);
-  text('Using p5.Oscillator (Sine Wave)', width / 2, 90);
-  
+  text("Using p5.Oscillator (Sine Wave)", width / 2, 90);
+
   // Draw instruction
   if (!isPlaying) {
     fill(255);
     textSize(20);
-    text('Click to play!', width / 2, height / 2);
+    text("Click to play!", width / 2, height / 2);
   } else {
     // Show current note
     fill(255, 215, 0);
@@ -71,35 +98,35 @@ function draw() {
     if (currentNote < melody.length) {
       text(melody[currentNote][0], width / 2, height / 2);
     }
-    
+
     // Draw progress bar
     let progress = currentNote / melody.length;
     let barWidth = width * 0.8;
     let barHeight = 20;
     let barX = width * 0.1;
     let barY = height - 60;
-    
+
     noFill();
     stroke(100);
     strokeWeight(2);
     rect(barX, barY, barWidth, barHeight, 10);
-    
+
     fill(255, 215, 0);
     noStroke();
     rect(barX, barY, barWidth * progress, barHeight, 10);
   }
-  
+
   // Handle playing
   if (isPlaying) {
     let elapsed = millis() - noteStartTime;
-    
+
     if (currentNote < melody.length) {
       let [note, duration] = melody[currentNote];
-      
+
       if (elapsed >= duration) {
         currentNote++;
         noteStartTime = millis();
-        
+
         if (currentNote < melody.length) {
           playNote(melody[currentNote][0]);
         } else {
@@ -136,7 +163,7 @@ function playNote(note) {
   let freq = noteFreqs[note];
   osc.freq(freq, 0.05);
   osc.amp(0.3, 0.05);
-  
+
   // Create particles at note position
   for (let i = 0; i < 5; i++) {
     particles.push(new Particle(width / 2, height / 2));
@@ -154,14 +181,14 @@ class Particle {
     this.size = random(5, 15);
     this.hue = random(360);
   }
-  
+
   update() {
     this.x += this.vx;
     this.y += this.vy;
     this.vy += 0.1; // gravity
     this.alpha -= 3;
   }
-  
+
   display() {
     colorMode(HSB, 360, 100, 100, 255);
     fill(this.hue, 80, 100, this.alpha);
@@ -169,7 +196,7 @@ class Particle {
     circle(this.x, this.y, this.size);
     colorMode(RGB, 255);
   }
-  
+
   isDead() {
     return this.alpha <= 0;
   }
